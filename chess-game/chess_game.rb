2de63dde64729a40c6d97c579f63eb4265948f80
Alexey Vorobyov <avorobyov@gocardless.com>
2024-04-27 17:40:07 +0300

    chess_game

---
+++ chess-game/chess_game.rb
@@ -0,0 +1,21 @@
+module Chess
+  FILES = 'A'..'H'
+  RANKS = 1..8
+
+  def self.valid_square?(rank, file)
+    RANKS.include?(rank) && FILES.include?(file)
+  end
+
+  def self.nick_name(first_name, last_name)
+    first_name[..1].upcase + last_name[-2..].upcase
+  end
+
+  def self.move_message(first_name, last_name, square)
+    nickname = nick_name(first_name, last_name)
+    if valid_square?(square[1].to_i, square[0])
+      "#{nickname} moved to #{square}"
+    else
+      "#{nickname} attempted to move to #{square}, but that is not a valid square"
+    end
+  end
+end

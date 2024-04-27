34698b4aa2b0a15d5b6a97ac9d52af3a8aeb7076
Alexey Vorobyov <avorobyov@gocardless.com>
2024-04-27 12:23:47 +0300

    few learnings from others

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -4,6 +4,7 @@ class Phrase
   end
 
   def word_count
+    return word_count_improved
     # split by any NON-alphanumeric characters leave out apostrophes
     # remove apostrophes from beginning and end of words
     # filter out empty strings
@@ -16,5 +17,17 @@ class Phrase
       counts[word] += 1
     end
     counts
+  end
+
+
+  def word_count_improved
+    # https://exercism.org/tracks/ruby/exercises/word-count/solutions/erikpivotal
+    # match words with boundary
+    words = @phrase.downcase.scan(/\b[\w']+\b/)
+    # default value for hash is important, since math operations will follow
+    words.each_with_object(Hash.new(0)) { |string, hash| hash[string] += 1 }
+
+    # Returns a hash containing the counts of equal elements:
+    # @phrase.downcase.scan(/\b[\w']+\b/).tally
   end
 end

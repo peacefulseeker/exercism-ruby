725689c3bbb4bece548472c3c1c2d5a3686cf0bd
Alexey Vorobyov <avorobyov@gocardless.com>
2024-04-28 13:29:40 +0300

    learnings

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -1,5 +1,11 @@
 class Acronym
   def self.abbreviate(phrase)
-    phrase.split(/[^a-zA-Z0-9']/).map { |word| word[0] }.join.upcase
+    # phrase.split(/[^a-zA-Z0-9']/).map { |word| word[0] }.join.upcase
+
+    # https://exercism.org/tracks/ruby/exercises/acronym/solutions/dmunoz-10
+    # phrase.split(/\W/).map(&:chr).join.upcase
+
+    # https://exercism.org/tracks/ruby/exercises/acronym/solutions/dmunoz-10
+    phrase.scan(/\b\w/).join.upcase
   end
 end
--- notes.md
+++ notes.md
@@ -8,3 +8,12 @@
 ```ruby
 colors[..1] == colors.take(2)  # take first 2 items from array
 ```
+
+### String & Regex
+```shell
+\b # matches word boundaries
+\w # matches any word character
+\W # matches any non-word character
+String.chr # returns the first character of a string
+String.upcase # returns the uppercase version of a string
+```

84945170994236f2ee5f72e0ac831d3bee710700
Alexey Vorobyov <avorobyov@gocardless.com>
2024-04-29 19:24:14 +0300

    learnings
    Array.each_cons method is just beatiful is not it?
    
    prefer to be explicit regarding potential exceptions raised.

--- series/series.rb
+++ series/series.rb
@@ -1,16 +1,20 @@
 class Series
-  attr_reader :string
-
   def initialize(string)
     @string = string
   end
 
-  def slices(slices_num)
-    raise ArgumentError if string.empty? || slices_num <= 0 || slices_num >= 6
+  def slices(num)
+    raise ArgumentError if num > @string.length || num.negative? || num.zero?
 
-    mapping = string.chars.map.with_index do |_, index|
-      string[index..index + slices_num - 1]
-    end
-    mapping.filter { |i| i.size == slices_num }
+    # mapping = @string.chars.map.with_index do |_, index|
+    #   @string[index..index + num - 1]
+    # end
+    # mapping.select { |i| i.size == num }
+
+    # this might raise ArgumentError(invalid size) in case of wrong num spec
+    @string         # assuming n == 2 :
+      .each_char    # %w(s t r i n g)
+      .each_cons(num) # [%w(s t), %w(t r), %w(r i), %w(i n), %w(n g)]
+      .map(&:join) # %w(st tr ri in ng)
   end
 end

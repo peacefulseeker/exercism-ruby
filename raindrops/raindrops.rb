7402043b4512602dd1b6efa547ce5dbbed490513
Alexey Vorobyov <avorobyov@gocardless.com>
2024-04-30 18:39:17 +0300

    extendible solution by exercism founder

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,12 +1,22 @@
-class Raindrops
-  def self.convert(number)
-    string = ''
-    # alternative string compsing
-    # string << 'Pling' if (number % 3).zero?
-    string += 'Pling' if (number % 3).zero?
-    string += 'Plang' if (number % 5).zero?
-    string += 'Plong' if (number % 7).zero?
+# class Raindrops
+#   def self.convert(number)
+#     string = ''
+#     # alternative string compsing
+#     # string << 'Pling' if (number % 3).zero?
+#     string += 'Pling' if (number % 3).zero?
+#     string += 'Plang' if (number % 5).zero?
+#     string += 'Plong' if (number % 7).zero?
 
-    string.empty? ? number.to_s : string
+#     string.empty? ? number.to_s : string
+#   end
+# end
+
+# founder approach, extendible
+# https://exercism.org/tracks/ruby/exercises/raindrops/solutions/iHiD
+module Raindrops
+  SOUNDS = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }.freeze
+  def self.convert(num)
+    rhythm = SOUNDS.select { |key, _| (num % key).zero? }.values
+    rhythm.empty? ? num.to_s : rhythm.join
   end
 end

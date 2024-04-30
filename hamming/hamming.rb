da9de54e93c64bb96c7f20ebb285314607aa7526
Alexey Vorobyov <avorobyov@gocardless.com>
2024-04-30 09:06:34 +0300

    learnings

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,10 +2,23 @@ class Hamming
   def self.compute(strand1, strand2)
     raise ArgumentError if strand1.length != strand2.length
 
-    counter = 0
-    strand1.each_char.with_index do |char, index|
-      counter += 1 if char != strand2[index]
-    end
-    counter
+    # brute force
+    # counter = 0
+    # strand1.each_char.with_index do |char, index|
+    #   counter += 1 if char != strand2[index]
+    # end
+    # counter
+
+    #  array pair zipping
+    # zipping each item of one array with the corresponding item of the other array
+    # using count block method to compare each pair and count diffs
+    # https://exercism.org/tracks/ruby/exercises/hamming/solutions/vrand
+    strand1.chars.zip(strand2.chars).count { |a, b| a != b }
+
+    # Counting diffs by array item references(using indices)
+    # https://exercism.org/tracks/ruby/exercises/hamming/solutions/dzivalli
+    # (0...strand1.length).count { |i| strand1[i] != strand2[i] }
+    # same as
+    # strand1.length.times.count { |i| strand1[i] != strand2[i] }
   end
 end

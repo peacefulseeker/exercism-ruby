7d88a448aed643ff2062fecdcf86a1337ecc5219
Alexey Vorobyov <avorobyov@gocardless.com>
2024-04-30 21:29:27 +0300

    luhn

---
+++ luhn/luhn.rb
@@ -0,0 +1,19 @@
+module Luhn
+  def self.valid?(string)
+    return false if string.scan(/[^0-9\s]/).any?
+
+    digits = string.scan(/\d/).map(&:to_i)
+    return false if digits.length < 2
+
+    doubled = digits.reverse.map.with_index do |digit, index|
+      if index.even?
+        digit
+      else
+        doubled = digit * 2
+        doubled < 10 ? doubled : doubled - 9
+      end
+    end
+
+    (doubled.reverse.sum % 10).zero?
+  end
+end

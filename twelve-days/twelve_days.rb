c18dfb8030bc7da568810bf9d21600679278b85b
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-01 20:34:19 +0300

    learnings
    - reference by name is also possible in ruby
    https://ruby-doc.org/3.2.3/format_specifications_rdoc.html#label-Reference+by+Name
    - inspiratios from other auhtor - mainly about proper namings.
        - using take and flat_map is amazing!
        - also destructuring items from array..

--- twelve-days/twelve_days.rb
+++ twelve-days/twelve_days.rb
@@ -1,7 +1,7 @@
 class TwelveDays
-  INTRO = 'On the %s day of Christmas my true love gave to me: %s.'.freeze
+  VERSE = 'On the %<day>s day of Christmas my true love gave to me: %<gifts>s.'.freeze
   DAYS = %w[first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth].freeze
-  PARTIALS = [
+  GIFTS = [
     'a Partridge in a Pear Tree',
     'two Turtle Doves',
     'three French Hens',
@@ -18,12 +18,63 @@ class TwelveDays
 
   def self.song
     song = DAYS.map.with_index do |day, index|
-      partials = PARTIALS[0..index].reverse
-      partials[-1] = "and #{partials[-1]}" if index.positive?
-      partials = partials.join(', ')
-      format(INTRO, day, partials)
+      gifts = GIFTS[0..index].reverse
+      gifts[-1] = "and #{gifts[-1]}" if index.positive?
+      gifts = gifts.join(', ')
+      format(VERSE, day:, gifts:)
     end
 
-    song.join("\n\n") + "\n"
+    "#{song.join("\n\n")}\n"
+  end
+end
+
+# beautiful 👏
+# https://exercism.org/tracks/ruby/exercises/twelve-days/solutions/shioimm
+class TwelveDays__
+  GIFTS = [
+    { first:    'a Partridge in a Pear Tree' },
+    { second:   'two Turtle Doves' },
+    { third:    'three French Hens' },
+    { fourth:   'four Calling Birds' },
+    { fifth:    'five Gold Rings' },
+    { sixth:    'six Geese-a-Laying' },
+    { seventh:  'seven Swans-a-Swimming' },
+    { eighth:   'eight Maids-a-Milking' },
+    { ninth:    'nine Ladies Dancing' },
+    { tenth:    'ten Lords-a-Leaping' },
+    { eleventh: 'eleven Pipers Piping' },
+    { twelfth:  'twelve Drummers Drumming' }
+  ].freeze
+
+  def self.song
+    # TODO: how's that called? I assume the purpose of method
+    # to avoid creating singleton methods every time
+    new.song
+  end
+
+  def song
+    GIFTS.size.times.map { |index| verse(index) }.join("\n")
+  end
+
+  private
+
+  def verse(index)
+    "On the #{day(index)} day of Christmas my true love gave to me: #{to_phrase(gifts(index))}.\n"
+  end
+
+  def day(index)
+    GIFTS[index].keys.first.to_s
+  end
+
+  def gifts(index)
+    GIFTS.take(index + 1).flat_map(&:values).reverse
+  end
+
+  def to_phrase(gifts)
+    *gifts, last_gift = gifts
+    # first gift only on first row
+    return last_gift if gifts.empty?
+
+    "#{gifts.join(', ')}, and #{last_gift}"
   end
 end

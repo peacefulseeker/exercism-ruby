19e2d10bea1e4cf59ce00388f0f6fdcc7aab2b75
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-01 09:41:16 +0300

    learnings
    liked the approach of generating
    a value for each letter based on
    array based values
    
    https://exercism.org/tracks/ruby/exercises/scrabble-score/solutions/Gao-Jun

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -1,22 +1,38 @@
 class Scrabble
   LETTER_SCORES = {
+    # rubocop:disable Layout/HashAlignment
     %w[A E I O U L N R S T] => 1,
-    %w[D G] => 2,
-    %w[B C M P] => 3,
-    %w[F H V W Y] => 4,
-    %w[K] => 5,
-    %w[J X] => 8,
-    %w[Q Z] => 10
+    %w[D G]                 => 2,
+    %w[B C M P]             => 3,
+    %w[F H V W Y]           => 4,
+    %w[K]                   => 5,
+    %w[J X]                 => 8,
+    %w[Q Z]                 => 10
   }.freeze
+  # rubocop:enable Layout/HashAlignment
+
+  # for each letter in the string, generate a hash with a value
+  # https://exercism.org/tracks/ruby/exercises/scrabble-score/solutions/Gao-Jun
+  VALUES = {
+    1 => %w[A E I O U L N R S T],
+    2 => %w[D G],
+    3 => %w[B C M P],
+    4 => %w[F H V W Y],
+    5 => %w[K],
+    8 => %w[J X],
+    10 => %w[Q Z]
+  }.flat_map { |v, chars| chars.map { |char| [char, v] } }.to_h
 
   def initialize(string)
     @string = string
   end
 
   def score
-    @string.upcase.chars.sum do |char|
-      char_key = LETTER_SCORES.keys.find { |k| k.include?(char) }
-      LETTER_SCORES[char_key]
-    end
+    # @string.upcase.chars.sum do |char|
+    #   char_key = LETTER_SCORES.keys.find { |k| k.include?(char) }
+    #   VALUES[char_key]
+    # end
+
+    @string.upcase.chars.sum { |char| VALUES[char] }
   end
 end

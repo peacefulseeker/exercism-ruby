fe68843b4f0d2044e8f625ed609b330d8b5193eb
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-03 11:12:19 +0300

    tournament iter 1

---
+++ tournament/tournament.rb
@@ -0,0 +1,82 @@
+require 'ostruct'
+
+class Tournament
+  module Constants
+    RESULT = [
+      LOSS = 'loss',
+      DRAW = 'draw',
+      WIN = 'win'
+    ].freeze
+  end
+
+  RESULT_TO_POINTS = {
+    Constants::WIN => 3,
+    Constants::DRAW => 1,
+    Constants::LOSS => 0
+  }.freeze
+
+  RESULT_TO_POINTS_FOR_TEAM2 = {
+    Constants::WIN => 0,
+    Constants::DRAW => 1,
+    Constants::LOSS => 3
+  }.freeze
+
+  def initialize
+    @teams = {}
+  end
+
+  def self.tally(input)
+    new.tally(input)
+  end
+
+  def tally(input)
+    rows = input.split(/\n/)
+    rows.each do |row|
+      team1_name, team2_name, result = row.split(';')
+
+      team1 = get_or_create_team(team1_name)
+      team2 = get_or_create_team(team2_name)
+
+      # TODO: make dry / optimal
+      team1.points += RESULT_TO_POINTS[result]
+      team1.matches += 1
+      team1.draws += result == Constants::DRAW ? 1 : 0
+      team1.wins += result == Constants::WIN ? 1 : 0
+      team1.losses += result == Constants::LOSS ? 1 : 0
+
+      team2.points += RESULT_TO_POINTS_FOR_TEAM2[result]
+      team2.matches += 1
+      team2.draws += result == Constants::DRAW ? 1 : 0
+      team2.wins += [Constants::WIN, Constants::DRAW].include?(result) ? 0 : 1
+      team2.losses += [Constants::LOSS, Constants::DRAW].include?(result) ? 0 : 1
+    end
+
+    # TODO: sort in place?
+    teams_sorted = teams.sort_by { |name, result| [-result.points, name] }.to_h
+
+    result = "#{'Team'.ljust(30)} | MP |  W |  D |  L |  P\n"
+    # TODO: make it more readable?
+    teams_sorted.each do |team|
+      result << "#{team[0].ljust(30)} | #{team[1].matches.to_s.rjust(2)} | #{team[1].wins.to_s.rjust(2)} | #{team[1].draws.to_s.rjust(2)} | #{team[1].losses.to_s.rjust(2)} | #{team[1].points.to_s.rjust(2)}\n"
+    end
+
+    result
+  end
+
+  private
+
+  attr_reader :teams
+
+  def get_or_create_team(name)
+    return teams[name] if teams.key?(name)
+
+    teams[name] = OpenStruct.new({
+                                   points: 0,
+                                   matches: 0,
+                                   wins: 0,
+                                   losses: 0,
+                                   draws: 0
+                                 })
+    teams[name]
+  end
+end

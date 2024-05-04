ad90f275f07978a266d663f73974abd9b9b2c193
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-04 17:24:05 +0300

    learnings
    String.tr -> translate characters 1 by 1

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,11 +1,13 @@
 class Complement
-  DNA_TO_RNA = {
-    G: :C,
-    C: :G,
-    T: :A,
-    A: :U
-  }
+  # DNA_TO_RNA = {
+  #   G: :C,
+  #   C: :G,
+  #   T: :A,
+  #   A: :U
+  # }
   def self.of_dna(dna)
-    dna.upcase.chars.map { |char| DNA_TO_RNA[char.to_sym] }.join
+    # dna.upcase.chars.map { |char| DNA_TO_RNA[char.to_sym] }.join
+
+    dna.tr('GCTA', 'CGAU')
   end
 end

42f3c280899d6333fa34a6f8b638dfdd192ce8cd
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-04 17:05:59 +0300

    dna -> rna

---
+++ rna-transcription/rna_transcription.rb
@@ -0,0 +1,11 @@
+class Complement
+  DNA_TO_RNA = {
+    G: :C,
+    C: :G,
+    T: :A,
+    A: :U
+  }
+  def self.of_dna(dna)
+    dna.upcase.chars.map { |char| DNA_TO_RNA[char.to_sym] }.join
+  end
+end

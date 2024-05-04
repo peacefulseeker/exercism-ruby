b6485186c05802bb97c09a4da8f260eaee0cd9cd
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-04 17:57:48 +0300

    hacky leap?

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,7 +1,14 @@
+require 'date'
+
 class Year
-  def self.leap?(year)
+  def self._leap?(year)
     return unless (year % 4).zero?
 
     (year % 100).zero? ? (year % 400).zero? : true
+  end
+
+  def self.leap?(year)
+    date = Date.new(year, 2, 28)
+    (date + 1).day == 29
   end
 end

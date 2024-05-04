17317931671f39edf58e03b47764e98c8546774a
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-04 12:43:50 +0300

    learnings
    - quite smart: using scan + uniq length

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -2,6 +2,9 @@ class Pangram
   LETTERS = ('a'..'z').to_a
 
   def self.pangram?(sentence)
-    LETTERS.all? { |letter| sentence.downcase.include?(letter) }
+    # LETTERS.all? { |letter| sentence.downcase.include?(letter) }
+
+    # https://exercism.org/tracks/ruby/exercises/pangram/solutions/frostyblok
+    sentence.downcase.scan(/[a-z]/).uniq.length == LETTERS.size
   end
 end

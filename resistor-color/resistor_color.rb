61e854143ecebf5010a2e135c6a5cc1e250bbe8b
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-04 12:48:53 +0300

    safeguarding

--- resistor-color/resistor_color.rb
+++ resistor-color/resistor_color.rb
@@ -2,6 +2,6 @@ class ResistorColor
   COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
 
   def self.color_code(color)
-    COLORS.index(color)
+    COLORS.index(color.downcase)
   end
 end

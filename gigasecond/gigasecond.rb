944c14c2021a88fb3ee604cd6a636f0664abd374
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-04 12:28:54 +0300

    learning

--- gigasecond/gigasecond.rb
+++ gigasecond/gigasecond.rb
@@ -3,6 +3,8 @@ class Gigasecond
   GIGASECOND = 1_000_000_000
 
   def self.from(date)
-    Time.at(date.to_i + GIGASECOND)
+    # Time.at(date.to_i + GIGASECOND)
+    # will handle conversion automatically
+    date + GIGASECOND
   end
 end

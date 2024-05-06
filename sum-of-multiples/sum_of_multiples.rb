258612687231ad443b1b1e8310a3badd307f1ad0
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-06 12:28:34 +0300

    learnings
    - good reminder about exclusive range
    and conditional selector among range of nums

--- sum-of-multiples/sum_of_multiples.rb
+++ sum-of-multiples/sum_of_multiples.rb
@@ -1,13 +1,20 @@
 class SumOfMultiples
-  def initialize(*multiples)
-    @multiples = multiples
+  def initialize(*factors)
+    @factors = factors
   end
 
   def to(limit)
-    result = @multiples.map do |multiple|
-      (1..limit - 1).select { |n| (n % multiple).zero? }
-    end
+    # result = @factors.map do |multiple|
+    #   (1...limit).select { |n| (n % multiple).zero? }
+    # end
 
-    result.flatten.uniq.sum
+    # result.flatten.uniq.sum
+
+    # https://exercism.org/tracks/ruby/exercises/sum-of-multiples/solutions/ErikSchierboom
+    # select among range of numbers, instead of mapping factors.
+    # only those which can be divided by any of the factors without a remainder
+    (1...limit)
+      .select { |n| @factors.any? { |factor| (n % factor).zero? } }
+      .sum
   end
 end

57897d09a309f3dad00adfb19f0cf41d32f40402
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-08 10:34:00 +0300

    learnings
    - array.compact removes nil
    - << same as push both fo arrays / strings.
    - each_with_object  + recursive as option

--- flatten-array/flatten_array.rb
+++ flatten-array/flatten_array.rb
@@ -6,6 +6,7 @@ class FlattenArray
   def flatten(array)
     # easy
     # array.flatten.reject(&:nil?)
+    # array.flatten.compact
 
     iterate_array(array)
   end
@@ -15,10 +16,21 @@ class FlattenArray
       if item.is_a? Array
         iterate_array(item, flattened)
       else
-        flattened.push(item) unless item.nil?
+        flattened << item unless item.nil?
       end
     end
 
     flattened
+  end
+
+  # https://exercism.org/tracks/ruby/exercises/flatten-array/solutions/toyseed
+  def iterate_array2(array)
+    array.each_with_object([]) do |item, flattened|
+      if item.is_a? Array
+        flattened.push(*iterate_array(item))
+      else
+        flattened << item unless item.nil?
+      end
+    end
   end
 end

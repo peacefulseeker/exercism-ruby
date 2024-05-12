024810b9919ecfdaedea367da0746bca7d1c3018
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-12 11:54:58 +0300

    Learnings
    - can destruct args in block itself

--- etl/etl.rb
+++ etl/etl.rb
@@ -1,7 +1,6 @@
 class ETL
   def self.transform(old)
-    old.each_with_object({}) do |item, transformed|
-      key, letters = item
+    old.each_with_object({}) do |(key, letters), transformed|
       letters.each { |letter| transformed[letter.downcase] = key }
     end
   end

ea0c4572048e7d1b28e4a8256e499492b6b9fdf4
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-15 10:03:07 +0300

    Learnings
    - =~ string match
    - more elegant argument error
    - naming is hard
    - count exists on String as well

--- nucleotide-count/nucleotide_count.rb
+++ nucleotide-count/nucleotide_count.rb
@@ -1,18 +1,18 @@
 class Nucleotide
-  ALLOWED = %w[A C G T].freeze
+  NUCLEOTIDES = %w[A C G T].freeze
+  # dynamic way of generating 0 for each nucleotide in hash
+  # EMPTY_HISTOGRAM = NUCLEOTIDES.zip([0] * NUCLEOTIDES.size).to_h
 
-  def self.from_dna(dna)
-    raise ArgumentError unless dna.chars.all? { |char| ALLOWED.include?(char) }
+  def self.from_dna(strand)
+    # raise ArgumentError unless strand.chars.all? { |char| NUCLEOTIDES.include?(char) }
+    # =~ will return the position of the element which does not match the ones in the group
+    raise NucleotideError if strand =~ /[^GTAC]/
 
-    new(dna).from_dna
+    new(strand)
   end
 
-  def initialize(dna)
-    @dna = ALLOWED.each_with_object({}) { |char, hash| hash[char] = dna.chars.count(char) }
-  end
-
-  def from_dna
-    self
+  def initialize(strand)
+    @dna = NUCLEOTIDES.each_with_object({}) { |char, hash| hash[char] = strand.count(char) }
   end
 
   def count(char)
@@ -21,5 +21,12 @@ class Nucleotide
 
   def histogram
     @dna
+  end
+end
+
+# kotp: https://exercism.org/tracks/ruby/exercises/nucleotide-count/solutions/kotp
+class NucleotideError < ArgumentError
+  def message
+    message || 'Given strand is does not look like a nucleotide strand'
   end
 end

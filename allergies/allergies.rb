3bb7a41e444bcc8d946ef937170c21dadc3af430
Alexey Vorobyov <avorobyov@gocardless.com>
2024-05-20 11:04:05 +0300

    Learnings
    - reminder about refine + using, not to amend global modules for every case.
    - bitwise AND comparison
    - bit shifting

--- allergies/allergies.rb
+++ allergies/allergies.rb
@@ -1,14 +1,29 @@
+# https://exercism.org/tracks/ruby/exercises/allergies/solutions/ajoshguy
+module Bits
+  refine Integer do
+    # bite shifting alternative
+    # 1 << 1 => 2
+    # 1 << 2 => 4
+    # 1 << 3 => 8
+    def bit
+      # 1 << self
+      2**self
+    end
+  end
+end
+
 class Allergies
+  using Bits
   ALLERGIES = {
-    eggs: 1,
-    peanuts: 2,
-    shellfish: 4,
-    strawberries: 8,
-    tomatoes: 16,
-    chocolate: 32,
-    pollen: 64,
-    cats: 128
-  }.freeze
+    eggs: 0.bit,
+    peanuts: 1.bit,
+    shellfish: 2.bit,
+    strawberries: 3.bit,
+    tomatoes: 4.bit,
+    chocolate: 5.bit,
+    pollen: 6.bit,
+    cats: 7.bit
+  }.transform_keys(&:to_s).freeze
 
   def initialize(score)
     @next_allergy_value = 2 * ALLERGIES.values.max
@@ -16,19 +31,23 @@ class Allergies
   end
 
   def allergic_to?(allergy)
-    list.include?(allergy)
+    # list.include?(allergy)
+    # bitwise AND https://ruby-doc.org/3.2.3/Integer.html#method-i-26
+    @score & ALLERGIES[allergy] != 0
   end
 
   def list
-    result = []
-    allergies = ALLERGIES.select { |_, v| v <= @score }
-    allergies.reverse_each do |key, value|
-      if @score >= value
-        @score -= value
-        result << key.to_s
-      end
-    end
+    # result = []
+    # allergies = ALLERGIES.select { |_, v| v <= @score }
+    # allergies.reverse_each do |key, value|
+    #   if @score >= value
+    #     @score -= value
+    #     result << key
+    #   end
+    # end
 
-    result.reverse
+    # result.reverse
+
+    ALLERGIES.keys.select { |a| allergic_to?(a) }
   end
 end
--- allergies/allergies_test.rb
+++ allergies/allergies_test.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+# require_relative 'allergies'
 require_relative 'allergies'
 
 class AllergiesTest < Minitest::Test

f0dca64f58dd67cf7f205aa8c6a44dea3a116c8d
Alexey Vorobyov <avorobyov@gocardless.com>
2024-06-01 11:57:40 +0300

    Learnings
    advantages of dynamic languages take place here!
    -  attr_reader destructuring
    - init each attribute once only in initializer
    - private constant
    - one-liner defs
    
    https://exercism.org/tracks/ruby/exercises/dnd-character/solutions/veezus

--- dnd-character/dnd_character.rb
+++ dnd-character/dnd_character.rb
@@ -2,25 +2,32 @@ class DndCharacter
   BASE_HITPOINTS = 10
   ATTRIBUTES = %i[strength dexterity constitution intelligence wisdom charisma].freeze
 
-  ATTRIBUTES.each do |attribute|
-    define_method attribute do
-      instance_variable_get("@#{attribute}") || instance_variable_set("@#{attribute}", attribute_sum)
+  attr_reader(*ATTRIBUTES)
+
+  def initialize
+    ATTRIBUTES.each do |attribute|
+      instance_variable_set("@#{attribute}", attribute_sum)
     end
   end
+
+  def hitpoints
+    BASE_HITPOINTS + self.class.modifier(constitution)
+  end
+
+  def self.modifier(score)
+    ((score - BASE_HITPOINTS) / 2).round
+  end
+
+  private
+
+  ROLLS = 1..6
+
   def roll
-    rand(1..6)
+    rand(ROLLS)
   end
 
   def attribute_sum
     # 1..3 works as well, an extra test would not hurt to ensure 4 dices rolled?
     (1..4).map { roll }.sort { |a, b| b <=> a }.take(3).sum
-  end
-
-  def hitpoints
-    BASE_HITPOINTS + DndCharacter.modifier(constitution)
-  end
-
-  def self.modifier(score)
-    ((score - BASE_HITPOINTS) / 2).round
   end
 end

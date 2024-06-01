09a0be805b875f1b3969dc56eaa84d9afe3393cd
Alexey Vorobyov <avorobyov@gocardless.com>
2024-06-01 17:38:55 +0300

    Learnings
    - block.call vs yield
    yield is more implicit then block.call, but more
    clean at the same time...

--- list-ops/list_ops.rb
+++ list-ops/list_ops.rb
@@ -1,53 +1,73 @@
 class ListOps
   def self.arrays(arr)
     counter = 0
+
     arr.each do |_|
       counter += 1
     end
+
     counter
   end
 
   def self.reverser(arr)
     reversed = []
+
     arr.size.times do |index|
       reversed[index] = arr[-(index + 1)]
     end
+
     reversed
   end
 
   def self.concatter(arr1, arr2)
-    [*arr1, *arr2]
+    concatted = []
+
+    arr1.each do |element|
+      concatted << element
+    end
+    arr2.each do |element|
+      concatted << element
+    end
+
+    concatted
   end
 
   def self.mapper(arr, &block)
     mapped = []
+
     arr.each do |element|
       mapped.push(block.call(element))
     end
+
     mapped
   end
 
   def self.filterer(arr, &block)
     filtered = []
+
     arr.each do |element|
       filtered.push(element) if block.call(element)
     end
+
     filtered
   end
 
   def self.sum_reducer(arr)
     sum = 0
+
     arr.each do |element|
       sum += element
     end
+
     sum
   end
 
   def self.factorial_reducer(arr)
-    fact = 1
+    factorial = 1
     arr.each do |element|
-      fact *= element
+      factorial *= element
     end
-    fact
+
+    factorial
   end
 end

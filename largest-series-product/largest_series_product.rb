ed0b49d56fa3d866e17d87dda3fbf3d3caf32c19
Alexey Vorobyov <avorobyov@gocardless.com>
2024-06-04 08:39:31 +0300

    Learnings
    keep things simple, combined preferably.
    - between? quite handy helper in this case.
    - Array.reduce == Array.inject
    it does need & sign in front
    of arg

--- largest-series-product/largest_series_product.rb
+++ largest-series-product/largest_series_product.rb
@@ -1,28 +1,15 @@
 class Series
-  class NegativeSpanError < ArgumentError; end
-  class UnexpectedCharError < ArgumentError; end
-  class EmptyStringError < ArgumentError; end
-  class ProductLargerStringLengthError < ArgumentError; end
-
+  # https://exercism.org/tracks/ruby/exercises/largest-series-product/solutions/CROUZET
   def initialize(string)
-    @string = validate_string(string)
+    raise ArgumentError if string.match?(/\D/)
+
+    @string = string
   end
 
   def largest_product(span)
-    validate_span(span)
+    # covers empy case, negative case or larger span than string length case
+    raise ArgumentError unless span.between?(0, @string.length)
 
-    @string.chars.each_cons(span).map { |slice| slice.map(&:to_i).reduce(&:*) }.max
-  end
-
-  def validate_string(string)
-    raise UnexpectedCharError unless (string =~ /\D/).nil?
-    raise EmptyStringError if string.empty?
-
-    string
-  end
-
-  def validate_span(span)
-    raise NegativeSpanError if span.negative?
-    raise ProductLargerStringLengthError if span > @string.length
+    @string.chars.each_cons(span).map { |slice| slice.map(&:to_i).reduce(:*) }.max
   end
 end

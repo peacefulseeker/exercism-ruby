27575747c6e158c52e8e16ae3bb72cdd64f040c2
Alexey Vorobyov <avorobyov@gocardless.com>
2024-06-16 11:59:15 +0300

    recursive approaches

--- prime-factors/prime_factors.rb
+++ prime-factors/prime_factors.rb
@@ -13,3 +13,30 @@ class PrimeFactors
     factors
   end
 end
+
+# recusrive approaches, but still prefer more readable loop to be honest
+# good to know about .then method block
+# https://exercism.org/tracks/ruby/exercises/prime-factors/solutions/CROUZET
+# class PrimeFactors
+#   def self.of(number)
+#     return [] unless number > 1
+
+#     next_factor(number).then do |factor|
+#       [factor] + PrimeFactors.of(number / factor)
+#     end
+#   end
+
+#   def self.next_factor(number)
+#     (2..number).find { |factor| number.modulo(factor).zero? }
+#   end
+# end
+
+# https://exercism.org/tracks/ruby/exercises/prime-factors/solutions/jchunky
+# class PrimeFactors
+#   def self.of(n)
+#     return [] if n < 2
+
+#     factor = (2..n).find { |i| n % i == 0 }
+#     [factor] + of(n / factor)
+#   end
+# end

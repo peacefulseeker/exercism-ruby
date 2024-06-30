54ffeaed58b84e4690763066ecec174cef2f5deb
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-06-30 11:45:56 +0300

    .to_s(2) / .digits work like a charm

--- eliuds-eggs/eliuds_eggs.rb
+++ eliuds-eggs/eliuds_eggs.rb
@@ -1,7 +1,9 @@
-class EliudsEggs
+class EliudsEggs_
   def self.egg_count(num)
     # simple
-    num.to_s(2).split('').map(&:to_i).sum
+    # num.to_s(2).split('').map(&:to_i).sum
+    # num.to_s(2).count('1')
+    num.digits(2).sum
 
     # custom
     # binaries = from_decimal(num, 2).sum
@@ -19,5 +21,19 @@ class EliudsEggs
       number, remainder = remainder.divmod(pow)
       numbers.push(number)
     end
+  end
+end
+
+# https://exercism.org/tracks/ruby/exercises/eliuds-eggs/solutions/juanpabloow
+# simple converter from decimal to binary
+class EliudsEggs
+  def self.egg_count(num)
+    binary = []
+    n = num
+    while n.positive?
+      binary << n % 2
+      n /= 2
+    end
+    binary.count(1)
   end
 end

4e64f0cddca8ffd5a9040b3c806169603e45adde
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-07-02 19:24:13 +0300

    complex numbers
    not my thing tbh. :D too deep

---
+++ complex-numbers/complex_numbers.rb
@@ -0,0 +1,53 @@
+class ComplexNumber
+  def initialize(real, imaginary = 0)
+    @real = real
+    @imaginary = imaginary
+  end
+
+  attr_accessor :real, :imaginary
+
+  def ==(other)
+    # @real == other.real && @imaginary == other.imaginary
+    (self - other).abs < 1e-15
+  end
+
+  def exp
+    ComplexNumber.new(Math.exp(@real)) * ComplexNumber.new(Math.cos(@imaginary), Math.sin(@imaginary))
+  end
+
+  def *(other)
+    real = @real * other.real - @imaginary * other.imaginary
+    imaginary = @imaginary * other.real + @real * other.imaginary
+
+    ComplexNumber.new(real, imaginary)
+  end
+
+  def abs
+    Math.sqrt(real**2 + imaginary**2)
+  end
+
+  def conjugate
+    ComplexNumber.new(real, -imaginary)
+  end
+
+  def /(other)
+    real = (@real * other.real + @imaginary * other.imaginary).fdiv(other.real**2 + other.imaginary**2)
+    imaginary = (@imaginary * other.real - @real * other.imaginary).fdiv(other.real**2 + other.imaginary**2)
+
+    ComplexNumber.new(real, imaginary)
+  end
+
+  def +(other)
+    real = @real + other.real
+    imaginary = @imaginary + other.imaginary
+
+    ComplexNumber.new(real, imaginary)
+  end
+
+  def -(other)
+    real = @real - other.real
+    imaginary = @imaginary - other.imaginary
+
+    ComplexNumber.new(real, imaginary)
+  end
+end

ff4f58807ca0c5b9477ee4644861bd85949f49de
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-07-02 13:54:32 +0300

    simple linked list
    tbh I'm too lazy to think on my own :D but remembered
    great tutorials by Lucas I went through in january/february this year
    
    https://www.youtube.com/watch?v=-cpmCrI1aEY
    
    good to know, there's no big
    necessity in having tail
    to reverse the linked list

--- simple-linked-list/simple_linked_list.rb
+++ simple-linked-list/simple_linked_list.rb
@@ -1,13 +1,14 @@
 class Element
   def initialize(datum)
     @datum = datum
-    @next = nil
   end
 
   attr_accessor :datum, :next
 end
 
-class SimpleLinkedList < Array
+# simple stupid approach...
+# couple of tests to ensure no inheritance used would not hurt
+class SimpleLinkedListArray < Array
   def initialize(array = [])
     elements = array.map { |datum| Element.new(datum) }
     super(elements)
@@ -15,5 +16,55 @@ class SimpleLinkedList < Array
 
   def to_a
     map(&:datum).reverse
+  end
+end
+
+class SimpleLinkedList
+  def initialize(array = [])
+    array.each do |item|
+      push(Element.new(item))
+    end
+  end
+
+  attr_reader :head
+
+  def push(element)
+    element.next = @head
+    @head = element
+    self
+  end
+
+  def pop
+    return nil if @head.nil?
+
+    element = @head
+    @head = @head.next
+    element.next = nil
+    element
+  end
+
+  def to_a
+    res = []
+    curr = @head
+    until curr.nil?
+      res << curr.datum
+      curr = curr.next
+    end
+    res
+  end
+
+  # interesting approach which allows not to introduce @tail
+  # https://exercism.org/tracks/ruby/exercises/simple-linked-list/solutions/kotp
+  def reverse!
+    previous = nil
+    pointer = @head
+    while pointer
+      temp = pointer.next
+      pointer.next = previous
+      previous = pointer
+      pointer = temp
+    end
+    @head = previous
+    self
   end
 end

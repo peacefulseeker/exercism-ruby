a7eccbb0a0552f507ae453169d58d7e77535fb85
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-07-03 15:49:59 +0300

    Learnings
    - method_missing alternative
    - &LETTER_TO_PLANT shortcut in hash mapping
    - flat_map even shorter vs with_object and pushing unpacking array items

--- kindergarten-garden/kindergarten_garden.rb
+++ kindergarten-garden/kindergarten_garden.rb
@@ -1,5 +1,5 @@
 class Garden
-  CHILDREN = %w[alice bob charlie david eve fred ginny harriet ileana joseph kincaid larry].freeze
+  CHILDREN = %i[alice bob charlie david eve fred ginny harriet ileana joseph kincaid larry].freeze
   LETTER_TO_PLANT = {
     'C' => :clover,
     'G' => :grass,
@@ -11,12 +11,25 @@ class Garden
     @plant_rows = garden.split "\n"
   end
 
-  CHILDREN.each.with_index do |child, index|
-    define_method child do
-      child_plant_range = index * 2..index * 2 + 1
-      @plant_rows.each.with_object([]) do |row, plants|
-        plants.push(*row.chars[child_plant_range].map { |letter| LETTER_TO_PLANT[letter] })
-      end
+  # CHILDREN.each.with_index do |child, index|
+  #   define_method child do
+  #     child_plant_range = index * 2..index * 2 + 1
+  #     @plant_rows.each.with_object([]) do |row, plants|
+  #       plants.push(*row.chars[child_plant_range].map(&LETTER_TO_PLANT))
+  #     end
+  #   end
+  # end
+
+  def respond_to_missing?(method)
+    CHILDREN.include?(method)
+  end
+
+  def method_missing(method)
+    index = CHILDREN.index(method)
+
+    child_plant_range = index * 2..index * 2 + 1
+    @plant_rows.flat_map do |row|
+      row.chars[child_plant_range].map(&LETTER_TO_PLANT)
     end
   end
 end

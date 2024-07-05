3573c19078be40a3244c72d7dbb46e7763b900f3
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-07-05 10:45:15 +0300

    cleanup
    TODO:
    - add more methods with clear function
    - add Frame class with pins values
    and all relevant methods. e.g. Frame.strike?, or Frame.completed?

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,22 +1,12 @@
 class Game
   class BowlingError < StandardError; end
 
+  BONUS_BALLS_AFTER_STRIKE = 2
+  BONUS_BALLS_AFTER_SPARE = 1
+
   def initialize
-    @pins = []
     @frames = []
     @frames_allowance = 10
-  end
-
-  def non_strike?(frame)
-    frame[0] != 10
-  end
-
-  def non_spare?(frame)
-    frame_completed?(frame) && frame[0] + frame[1] < 10
-  end
-
-  def frame_completed?(frame)
-    frame[0] && frame[1]
   end
 
   def roll(pins)
@@ -24,13 +14,17 @@ class Game
     frame_before_last = @frames[@frames.size - 2]
     raise Game::BowlingError unless pins.between?(0, 10)
     raise Game::BowlingError if @frames.size >= @frames_allowance && frame_completed?(last_frame)
-    raise Game::BowlingError if bonus_roll? && (non_strike?(last_frame) && non_spare?(last_frame))
-    raise Game::BowlingError if bonus_roll? && (spare?(frame_before_last) && !frame_completed?(last_frame))
-    raise Game::BowlingError if bonus_roll? && (strike?(frame_before_last) && last_frame[0] != 10 && pins == 10)
+    if bonus_roll? && (
+      (frame_completed?(last_frame) && !strike?(last_frame) && !spare?(last_frame)) ||
+         (spare?(frame_before_last) && !frame_completed?(last_frame)) ||
+         (strike?(frame_before_last) && last_frame[0] != 10 && pins == 10)
+    )
+      raise Game::BowlingError
+    end
 
     if pins == 10
       @frames << [10, 0]
-      @frames_allowance += 2 if tenth_frame?
+      @frames_allowance += BONUS_BALLS_AFTER_STRIKE if tenth_frame?
     elsif last_frame.nil? || (!last_frame[0].nil? && !last_frame[1].nil?)
       add_new_frame(pins)
     elsif last_frame[1].nil?
@@ -40,22 +34,10 @@ class Game
 
       @frames[@frames.size - 1][1] = pins
 
-      @frames_allowance += 1 if combo == 10 && tenth_frame?
+      @frames_allowance += BONUS_BALLS_AFTER_SPARE if combo == 10 && tenth_frame?
     else
       add_new_frame(pins)
     end
-  end
-
-  def add_new_frame(pins)
-    @frames << [pins, nil]
-  end
-
-  def bonus_roll?
-    @frames.size.between?(10, 11)
-  end
-
-  def tenth_frame?
-    @frames.size == 10
   end
 
   def score
@@ -72,6 +54,24 @@ class Game
     end
 
     score
+  end
+
+  private
+
+  def frame_completed?(frame)
+    frame[0] && frame[1]
+  end
+
+  def add_new_frame(pins)
+    @frames << [pins, nil]
+  end
+
+  def bonus_roll?
+    @frames.size.between?(10, 11)
+  end
+
+  def tenth_frame?
+    @frames.size == 10
   end
 
   def strike_bonus(frame_index)

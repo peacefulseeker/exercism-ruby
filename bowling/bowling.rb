0d64fc0c1e2d5dc9d434c2a2565c67a77af828ee
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-07-05 10:37:14 +0300

    too complicated. Should not be so.
    That's why engineering is in place.
    To actually think the design/implementation early and then proceed with solution.
    
    here again felt that I'm just covering failing tests cases which is obviously bad.

---
+++ bowling/bowling.rb
@@ -0,0 +1,110 @@
+class Game
+  class BowlingError < StandardError; end
+
+  def initialize
+    @pins = []
+    @frames = []
+    @frames_allowance = 10
+  end
+
+  def non_strike?(frame)
+    frame[0] != 10
+  end
+
+  def non_spare?(frame)
+    frame_completed?(frame) && frame[0] + frame[1] < 10
+  end
+
+  def frame_completed?(frame)
+    frame[0] && frame[1]
+  end
+
+  def roll(pins)
+    last_frame = @frames[@frames.size - 1]
+    frame_before_last = @frames[@frames.size - 2]
+    raise Game::BowlingError unless pins.between?(0, 10)
+    raise Game::BowlingError if @frames.size >= @frames_allowance && frame_completed?(last_frame)
+    raise Game::BowlingError if bonus_roll? && (non_strike?(last_frame) && non_spare?(last_frame))
+    raise Game::BowlingError if bonus_roll? && (spare?(frame_before_last) && !frame_completed?(last_frame))
+    raise Game::BowlingError if bonus_roll? && (strike?(frame_before_last) && last_frame[0] != 10 && pins == 10)
+
+    if pins == 10
+      @frames << [10, 0]
+      @frames_allowance += 2 if tenth_frame?
+    elsif last_frame.nil? || (!last_frame[0].nil? && !last_frame[1].nil?)
+      add_new_frame(pins)
+    elsif last_frame[1].nil?
+      first_throw = last_frame[0]
+      combo = first_throw + pins
+      raise Game::BowlingError if combo > 10
+
+      @frames[@frames.size - 1][1] = pins
+
+      @frames_allowance += 1 if combo == 10 && tenth_frame?
+    else
+      add_new_frame(pins)
+    end
+  end
+
+  def add_new_frame(pins)
+    @frames << [pins, nil]
+  end
+
+  def bonus_roll?
+    @frames.size.between?(10, 11)
+  end
+
+  def tenth_frame?
+    @frames.size == 10
+  end
+
+  def score
+    raise Game::BowlingError unless @frames.size.between?(10, @frames_allowance)
+    raise Game::BowlingError if (strike?(@frames[9]) || spare?(@frames[9])) && @frames.size < 11
+    raise Game::BowlingError if strike?(@frames[9]) && strike?(@frames[10]) && @frames.size < 12
+
+    score = 0
+
+    @frames.each.with_index do |frame, index|
+      score += 10 + strike_bonus(index) if strike?(frame)
+      score += 10 + spare_bonus(index) if spare?(frame)
+      score += open_frame_score(frame) if open_frame?(frame)
+    end
+
+    score
+  end
+
+  def strike_bonus(frame_index)
+    frame_next = @frames[frame_index + 1]
+    return 0 if frame_next.nil? || frame_index >= 9
+
+    frame_following_next = @frames[frame_index + 2]
+
+    sum = frame_next.sum
+    sum += frame_following_next[0] if frame_following_next && strike?(frame_next)
+    sum
+  end
+
+  def spare_bonus(frame_index)
+    frame_next = @frames[frame_index + 1]
+    return 0 if frame_next.nil? || frame_index >= 9
+
+    frame_next[0]
+  end
+
+  def open_frame_score(frame)
+    frame[0] + (frame[1] || 0)
+  end
+
+  def open_frame?(frame)
+    frame && frame[0] + (frame[1] || 0) < 10
+  end
+
+  def strike?(frame)
+    frame && frame[0] == 10
+  end
+
+  def spare?(frame)
+    !strike?(frame) && frame[0] + (frame[1] || 0) == 10
+  end
+end
